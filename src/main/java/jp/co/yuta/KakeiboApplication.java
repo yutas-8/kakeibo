656deafc33f6c8eecb9dd75dcbb5254d71057230
Yuta Sasaki <gh01.yuta@gmail.com>
2023-10-26 17:49:22 +0900

    first commit

---
+++ src/main/java/jp/co/yuta/KakeiboApplication.java
@@ -0,0 +1,13 @@
+package jp.co.yuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboApplication.class, args);
+	}
+
+}
